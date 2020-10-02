20718f7a3033a93ab57cd351d0d0af3cf71faccb
Caden Shepherd <caden.shepherd.643@k12.friscoisd.org>
2020-10-02 13:39:22 -0500

    Just trying to get this to work

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Shepherd, Caden M on 10/2/20.
+//  Copyright © 2020 Shepherd, Caden M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
